5c512a0d8124e20d31e38baa82729ef979c96463
Sara Harvey <saraharveynyc@gmail.com>
2018-09-25 16:00:45 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array, x)
   collection = []
   i = 0
   	while i < array.length 
-  	  collection << yield(array, x)
+  	  collection << yield(array)
     i += 1 
   	end
   	collection
